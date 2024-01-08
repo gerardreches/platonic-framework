15b74d38f5b1401dd54583ee457ac7401d9356d0
Gerard Reches <gerardreches@gmail.com>
2024-01-08 20:34:02 +1000

    Define parameters and return types according to documentation.

--- includes/Settings/Interface/Theme_Settings_Page_Rules.php
+++ includes/Settings/Interface/Theme_Settings_Page_Rules.php
@@ -2,8 +2,14 @@
 
 namespace Platonic\Framework\Settings\Interface;
 
-interface Theme_Settings_Page_Rules extends Settings_Page_Rules {
-	function on_theme_activation( $new_theme );
+use WP_Theme;
 
-	function on_theme_deactivation();
+interface Theme_Settings_Page_Rules extends Settings_Page_Rules {
+
+	function on_theme_activation( string $old_name, WP_Theme $old_theme ): void;
+
+	function on_theme_deactivation( string $new_name, WP_Theme $new_theme, WP_Theme $old_theme ): void;
+
+	function on_theme_deletion( string $stylesheet ): void;
+
 }
