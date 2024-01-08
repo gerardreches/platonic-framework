feca2cf8a41e414eb696ca594dbac70530c62252
Gerard Reches <gerardreches@gmail.com>
2024-01-08 20:31:10 +1000

    Add return types

--- includes/Settings/Interface/Plugin_Settings_Page_Rules.php
+++ includes/Settings/Interface/Plugin_Settings_Page_Rules.php
@@ -3,9 +3,11 @@
 namespace Platonic\Framework\Settings\Interface;
 
 interface Plugin_Settings_Page_Rules extends Settings_Page_Rules {
-	public function on_plugin_activation();
 
-	public function on_plugin_deactivation();
+	public function on_plugin_activation(): void;
 
-	public static function on_plugin_uninstall();
+	public function on_plugin_deactivation(): void;
+
+	public static function on_plugin_uninstall(): void;
+
 }
